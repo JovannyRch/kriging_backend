5258db360f32d7a526f0fb018aaeeb2786de86bc
JovannyRch <jovannyrch@gmail.com>
2023-10-18 20:22:11 -0600

    kriging_contour

--- app.py
+++ app.py
@@ -56,5 +56,36 @@ def get_semivariogram():
     return jsonify({"hs": OK.lags.tolist(), "semivariograms": OK.semivariance.tolist(), "image_base64": image_base64})
 
 
+@app.route('/kriging_contour', methods=['POST'])
+def get_kriging_contour():
+    
+    data = request.json
+    lons = np.array(data["lons"])
+    lats = np.array(data["lats"])
+    values = np.array(data["values"])
+
+    # Define la cuadrícula sobre la que se realizará el kriging
+    grid_lon = np.linspace(min(lons), max(lons), 50)
+    grid_lat = np.linspace(min(lats), max(lats), 50)
+
+    OK = OrdinaryKriging(lons, lats, values, variogram_model='spherical')
+    z, ss = OK.execute('grid', grid_lon, grid_lat)
+
+    
+
+    # Dibuja el mapa de contornos
+    fig, ax = plt.subplots()
+    c = ax.contourf(grid_lon, grid_lat, z, cmap="viridis")
+    fig.colorbar(c, ax=ax)
+
+    # Guarda la figura en un objeto BytesIO y luego convierte a base64
+    buf = BytesIO()
+    plt.savefig(buf, format="png")
+    base64_image = base64.b64encode(buf.getvalue()).decode('utf-8')
+    
+    return jsonify({"image_base64": base64_image})
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
